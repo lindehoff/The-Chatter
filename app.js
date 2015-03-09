844c52916195eb584e91a7d8ae4c982323c39445
lindehoff <jacob@cobmedia.se>
2015-03-09 16:10:25 +0100

    added routes for index and chatrooms

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var express = require('express'),
 	app = express(),
 	path = require('path');
 
-app.set('view', path.join(__dirname, 'views'));
-app.engine('html', require('hogen-express'));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 
+require('./routers/routers.js')(express, app);
+
+app.listen(3000, function() {
+	console.log('The Chatter is running on port: 3000')
+})
