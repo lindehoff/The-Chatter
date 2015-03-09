99d86fd8b7f10d7467d592fd93261f74c547a37c
lindehoff <jacob@cobmedia.se>
2015-03-09 15:47:33 +0100

    use hogan-express

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ var express = require('express'),
 	path = require('path');
 
 app.set('view', path.join(__dirname, 'views'));
+app.engine('html', require('hogen-express'));
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
+
