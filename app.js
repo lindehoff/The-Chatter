d429e5fef1c1363be419cef9add0c44b5239115e
lindehoff <jacob@cobmedia.se>
2015-03-09 16:38:23 +0100

    Added session managemnet and configuration modes

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 var express = require('express'),
 	app = express(),
-	path = require('path');
+	path = require('path'),
+	cookieParser = require('cookie-parser'),
+	session = require('express-session'),
+	config = require('./config/config.js'),
+	ConnectMongo = require('connect-mongo')(session);
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+var env = process.env.NODE_ENV || 'development';
+
+var env = process.env.NODE_ENV || 'development';
+if(env === 'development'){
+	app.use(session({
+		secret: config.sessionSecret, 
+		saveUninitialized:true, 
+		resave:true
+	}));
+}else{
+	app.use(session({
+		secret: config.sessionSecret, 
+		store: new ConnectMongo({
+			//url:config.dbURL,
+			mongoose_connection: mongoose.connections[0],
+			stringify:true
+		}),
+		saveUninitialized:true, 
+		resave:true
+	}));	
+}
 
 require('./routers/routers.js')(express, app);
 
